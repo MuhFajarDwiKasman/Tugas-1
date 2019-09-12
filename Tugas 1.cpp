e04e8b75500f1b1172de2d198ab9eb4d91e66f04
MuhFajarDwiKasman <55200456+MuhFajarDwiKasman@users.noreply.github.com>
2019-09-12 13:16:57 +0800

    Tugas Pertama

---
+++ Tugas 1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ 
+
+int main(){
+int A = 97, B = 141, C = 61, D = 1100001;
+
+cout<<"Nilai variabel nilai A adalah = "<<A<<endl;
+cout<<"Nilai variabel nilai B adalah = "<<B<<endl;
+cout<<"Nilai variabel nilai C adalah = "<<C<<endl;
+cout<<"Nilai variabel nilai D adalah = "<<D<<endl;
+return 0;
+}
